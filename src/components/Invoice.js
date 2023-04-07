b6ed16530e4c5d684d44cc5303b19d39e0ac35be
Cao Hoàng <caohoang2911@gmail.com>
2023-04-07 23:59:03 +0700

    update layout

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -5,14 +5,17 @@ const Invoice = ({ data }) => {
 
   
   const orderDetail = data?.data;
+  console.log(orderDetail, "orderDetail");
   const { header } = orderDetail || {}
   const { incurredPayment } = header || {};
   const { type, amount: amountIncurredPayment } = incurredPayment || {};
 
-  const { customer, employee } = header || {};
+  const { customer, employee, payment } = header || {};
   const { products, shippingPrice, shippingDiscount, invoice} = orderDetail?.deliveries?.[0] || {};
   const { fullAddress , code: deliveryCode } = orderDetail?.deliveries?.[0]?.store || {};
+  const { method }  =  payment || {};
 
+  const isPayOneLife = method?.method === 'ONELIFE';
 
   const invoiceCode = invoice?.code;
 
@@ -89,7 +92,7 @@ const Invoice = ({ data }) => {
     return (
       <div style={{
         textAlign: "center",
-        marginTop: "50px"
+        marginTop: "47px"
       }}>
         <div>{orderDetail?.error}</div>
       </div>
@@ -98,9 +101,9 @@ const Invoice = ({ data }) => {
     return (
       <div
         style={{
-          fontFamily: 'Lato, sans-serif',
-          fontSize: '16px',
-          lineHeight: '25px',
+          fontFamily: 'Inter',
+          fontSize: '20px',
+          lineHeight: '28px',
         }}
       >
         <div
@@ -109,11 +112,10 @@ const Invoice = ({ data }) => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            padding: '25px 0px',
           }}
           id="invoice-wp"
         >
-          <div id="invoice" style={{ padding: '15px', width: '452px', border: '1px solid #dfdfdf' }}>
+          <div id="invoice" style={{ width: '477px' }}>
             <div
               style={{
                 display: 'flex',
@@ -127,7 +129,6 @@ const Invoice = ({ data }) => {
                   flexDirection: 'column',
                   alignItems: 'center',
                   justifyContent: 'center',
-                  gap: '10px',
                 }}
               >
                 <div
@@ -135,7 +136,6 @@ const Invoice = ({ data }) => {
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
-                    gap: 8,
                   }}
                 >
                   <img src="https://oms.seedcom.me/logo-kfm.png" />
@@ -147,17 +147,20 @@ const Invoice = ({ data }) => {
                     alignItems: 'center',
                   }}
                 >
-                  <span style={{ width: '85%', marginBottom: '10px', textAlign: 'center' }}>
-                    CH: Kingfoodmart - {fullAddress}
+                  <span style={{ width: '85%', marginBottom: '23px', textAlign: 'center' }}>
+                    ĐC: Kingfoodmart - {fullAddress}
                   </span>
-                  <span>
+                  {/* <span>
                     Mã cửa hàng: {deliveryCode} · Mã NV: {employee?.employeeId || '--'}
+                  </span> */}
+                  <span>
+                    Điện thoại: 18006804
                   </span>
                 </div>
                 <div
-                  style={{ border: '1px dashed #000000', margin: '15px 0px', width: '100%' }}
+                  style={{ border: '1px dashed #000000', margin: '16px 0px', width: '100%' }}
                 ></div>
-                <h1 style={{ fontSize: '25px', fontWeight: 'bold', marginBottom: '12px' }}>
+                <h1 style={{ fontSize: '24px', margin: 0, fontWeight: 'bold',}}>
                   HOÁ ĐƠN BÁN HÀNG
                 </h1>
               </div>
@@ -165,19 +168,23 @@ const Invoice = ({ data }) => {
                 style={{
                   display: 'flex',
                   flexDirection: 'column',
+                  marginTop: "16px"
                 }}
               >
                 <span>Số HD: {invoiceCode || '--'}</span>
-                <span>
-                  Thời gian: {moment(orderDetail?.header?.orderTime).format('HH:mm DD/MM/YYYY')}
-                </span>
                 <span>Mã đơn hàng: {orderDetail?.header?.code}</span>
+                <span>
+                  Ngày {moment(orderDetail?.header?.orderTime).format('DD')} 
+                  tháng {moment(orderDetail?.header?.orderTime).format('MM')} 
+                  năm {moment(orderDetail?.header?.orderTime).format('YYYY')} 
+                  &nbsp;&nbsp;&nbsp; 
+                  Giờ {moment(orderDetail?.header?.orderTime).format('HH:mm')} 
+                </span>
                 <span>Khách hàng: {customer?.name}</span>
                 <span>
                   SDT:
                   {customer?.phone?.toString()?.slice(0, customer?.phone?.length - 3)} ***
                 </span>
-                <div style={{ margin: '15px 0px' }}></div>
                 <div
                   style={{ border: '1px dashed #000000', margin: '15px 0px', width: '100%' }}
                 ></div>
@@ -188,7 +195,7 @@ const Invoice = ({ data }) => {
                     marginTop: '10px',
                   }}
                 >
-                  <div style={{ width: '50%', fontWeight: 'bold', textAlign: 'left' }}>Đơn giá</div>
+                  <div style={{ width: '47%', fontWeight: 'bold', textAlign: 'left' }}>Đơn giá</div>
                   <div style={{ width: '25%', fontWeight: 'bold' }}>SL</div>
                   <div style={{ width: '25%', fontWeight: 'bold', textAlign: 'right' }}>
                     Thành tiền
@@ -216,7 +223,7 @@ const Invoice = ({ data }) => {
                           display: 'flex',
                         }}
                       >
-                        <div style={{ width: '50%' }}>
+                        <div style={{ width: '47%' }}>
                           {sellPrice === originPrice && (
                             <span>{new Intl.NumberFormat().format(originPrice) || 0}</span>
                           )}
@@ -244,7 +251,7 @@ const Invoice = ({ data }) => {
                           )}
                         </div>
                         <div style={{ width: '25%' }}>{product?.quantity}</div>
-                        <div style={{ width: '25%', textAlign: 'right' }}>
+                        <div style={{ width: '25%', textAlign: 'right', fontWeight: "bold" }}>
                           <span>
                             {' '}
                             {new Intl.NumberFormat().format(
@@ -272,7 +279,7 @@ const Invoice = ({ data }) => {
                   }}
                 >
                   <span>Tổng số lượng:</span>
-                  <span>{totalProduct}</span>
+                  <span style={{fontWeight: "bold"}}>{totalProduct}</span>
                 </div>
                 <div
                   style={{
@@ -281,7 +288,7 @@ const Invoice = ({ data }) => {
                   }}
                 >
                   <span>Tổng tiền hàng:</span>
-                  <span>{new Intl.NumberFormat().format(totalPrice) || 0}</span>
+                  <span style={{fontWeight: "bold"}}>{new Intl.NumberFormat().format(totalPrice) || 0}</span>
                 </div>
                 <div
                   style={{
@@ -290,7 +297,7 @@ const Invoice = ({ data }) => {
                   }}
                 >
                   <span>Phí vận chuyển:</span>
-                  <span>{new Intl.NumberFormat().format(shippingPriceTotal) || 0}</span>
+                  <span style={{fontWeight: "bold"}}>{new Intl.NumberFormat().format(shippingPriceTotal) || 0}</span>
                 </div>
                 <div
                   style={{
@@ -300,7 +307,7 @@ const Invoice = ({ data }) => {
                   }}
                 >
                   <span>Giảm phí vận chuyển:</span>
-                  <span>
+                  <span style={{fontWeight: "bold"}}>
                     {Boolean(shippingDiscountTotal) && '-'}
                     {new Intl.NumberFormat().format(shippingDiscountTotal) || 0}
                   </span>
@@ -326,9 +333,9 @@ const Invoice = ({ data }) => {
                   <span>Tộng cộng:</span>
                   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
                     {Boolean(saveMoney) && (
-                      <small>Tiết kiệm {new Intl.NumberFormat().format(saveMoney) || 0}</small>
+                      <span style={{fontSize: "16px"}}>Tiết kiệm {new Intl.NumberFormat().format(saveMoney) || 0}</span>
                     )}
-                    <span>{new Intl.NumberFormat().format(amountCaculator) || 0}</span>
+                    <span style={{fontWeight: "bold"}}>{new Intl.NumberFormat().format(amountCaculator) || 0}</span>
                   </div>
                 </div>
                 <div
@@ -338,18 +345,8 @@ const Invoice = ({ data }) => {
                     justifyContent: 'space-between',
                   }}
                 >
-                  <span>Tiền khách đưa:</span>
-                  <span>--</span>
-                </div>
-                <div
-                  style={{
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                  }}
-                >
-                  <span>Khách thanh toán thẻ Onelife :</span>
-                  <span>--</span>
+                  <span>{isPayOneLife ? "Khách thanh toán thẻ Onelife" : "Tiền khách đưa"}:</span>
+                  <span style={{fontWeight: "bold"}}>{new Intl.NumberFormat().format(amountCaculator) || 0}</span>
                 </div>
                 {renderIncurredPayment && (
                   <div
@@ -363,7 +360,7 @@ const Invoice = ({ data }) => {
                       <span>{renderIncurredPayment?.label}:</span>
                     </div>
                     <div style={{ textAlign: 'right' }}>
-                      <span>
+                      <span style={{fontWeight: "bold"}}>
                         {new Intl.NumberFormat().format(renderIncurredPayment?.amount || 0) || 0}
                       </span>
                     </div>
@@ -377,24 +374,10 @@ const Invoice = ({ data }) => {
                   }}
                 >
                   <div style={{ textAlign: 'right' }}>
-                    <span>Thanh toán bằng điểm (VNĐ):</span>
-                  </div>
-                  <div style={{ textAlign: 'right' }}>
-                    <span>--</span>
-                  </div>
-                </div>
-                <div
-                  style={{
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                  }}
-                >
-                  <div style={{ textAlign: 'right' }}>
                     <span>Tổng điểm còn lại:</span>
                   </div>
                   <div style={{ textAlign: 'right' }}>
-                    <span>--</span>
+                    <span style={{fontWeight: "bold"}}>--</span>
                   </div>
                 </div>
                 <div
@@ -408,7 +391,7 @@ const Invoice = ({ data }) => {
                     <span>Tiền còn trong thẻ:</span>
                   </div>
                   <div style={{ textAlign: 'right' }}>
-                    <span>--</span>
+                    <span style={{fontWeight: "bold"}}>--</span>
                   </div>
                 </div>
               </div>
@@ -417,10 +400,10 @@ const Invoice = ({ data }) => {
                   display: 'flex',
                   alignItems: 'center',
                   flexDirection: 'column',
-                  marginTop: '20px',
+                  marginTop: '24px',
                 }}
               >
-                <div style={{ width: '55%' }}>
+                <div style={{ width: '47%' }}>
                   <div style={{ border: '1px dashed #000000', margin: '15px 0px' }}></div>
                   <div style={{ border: '1px dashed #000000', margin: '15px 0px' }}></div>
                 </div>
@@ -429,37 +412,57 @@ const Invoice = ({ data }) => {
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
-                    margin: '18px 0px',
+                    marginTop: "24px"
                   }}
                 >
-                  <span style={{ fontSize: '20px', fontWeight: 'bold' }}>YÊU CẦU HỖ TRỢ</span>
-                  <span>
-                    <span>Liên hệ </span>
-                    <strong>1800 6804</strong>
-                  </span>
-                  <span>
-                    (<strong>7:00 - 21:00</strong> · Trừ CN và ngày lễ)
-                  </span>
+                  <span style={{ fontSize: '24px', fontWeight: 'bold' }}>ĐẶT HÀNG ONLINE</span>
+                  <span>Truy cập mua hàng online : <strong>onelife.vn</strong></span>
                 </div>
-                <div style={{ width: '55%' }}>
+                <div style={{ width: '47%', marginTop: "24px" }}>
                   <div style={{ border: '1px dashed #000000', margin: '15px 0px' }}></div>
                   <div style={{ border: '1px dashed #000000', margin: '15px 0px' }}></div>
                 </div>
               </div>
-  
+              <span style={{ fontSize: '24px', marginTop: "24px", textAlign: "center",  fontWeight: 'bold' }}>QUY ĐỊNH ĐỔI / TRẢ HÀNG HÓA</span>
               <div
                 style={{
                   display: 'flex',
                   gap: 5,
                   marginTop: '15px',
                   flexDirection: 'column',
+                  textAlign: "justify"
+                }}
+              > 
+              <span>- Thời gian giải quyết khiếu nại: 24h kể từ khi xuất hóa đơn</span>
+              <span>- Chấp nhận đổi trả khi sản phẩm còn nguyên bao bì (Áp dụng đối với các sản phẩm được phép đổi trả theo quy định của Kingfoodmart)</span>
+              <span>- Liên hệ CSKH: <strong>18006804</strong> (7:00 - 21:00 hàng ngày)</span>
+              </div>
+
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  marginTop: "24px"
+                }}
+              >
+                <div style={{ width: '47%' }}>
+                  <div style={{ border: '1px dashed #000000', margin: '15px 0px' }}></div>
+                  <div style={{ border: '1px dashed #000000', margin: '15px 0px' }}></div>
+                </div>
+              </div>                             
+              <div
+                style={{
+                  display: 'flex',
+                  marginTop: '24px',
+                  flexDirection: 'column',
                 }}
               >
                 <span style={{ fontSize: '24px', fontWeight: 'bold', textAlign: 'center' }}>
                   Tải ứng dụng OneLife để sử dụng thẻ ngay hôm nay
                 </span>
-                <span style={{ textAlign: 'center' }}>
-                  Dùng Thẻ OneLife - Kingfoodmart để hưởng <strong>Freeship</strong> và{' '}
+                <span style={{ textAlign: 'center', marginTop: "16px"}}>
+                  Dùng Thẻ OneLife - Kingfoodmart để hưởng <br/><strong>Freeship</strong> và{' '}
                   <strong>nhận thêm giá trị Thẻ nạp</strong>
                 </span>
               </div>
@@ -468,7 +471,7 @@ const Invoice = ({ data }) => {
               style={{
                 display: 'flex',
                 justifyContent: 'center',
-                marginTop: '10px',
+                marginTop: '16px',
               }}
             >
               <img src="https://oms.seedcom.me/qr.jpeg" width="158" height="158" />
